5fdccd140a17ba310b30557ab233b3d981c6da27
jweir136 <jweir136@yahoo.com>
2021-09-27 13:14:02 -0400

    Add the first draft of the basic template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent , pathMatch: 'full' },
+  { path: 'signup', component: SignupPageComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
